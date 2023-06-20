ecc218e485e68c9faa58b78f21277c7d1312efa0
Myandes <2295126@cegepoutaouais.qc.ca>
2023-06-20 14:06:14 -0400

    imgae fix

--- app/src/main/java/com/reytian/demolisteview/MonAdaptateur.kt
+++ app/src/main/java/com/reytian/demolisteview/MonAdaptateur.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Adapter
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.TextView
 
 class MonAdaptateur (var context : Activity, var liste : ArrayList<Etudiant>): ArrayAdapter<Etudiant>(context, R.layout.liste_test, liste) {
@@ -15,6 +16,7 @@ class MonAdaptateur (var context : Activity, var liste : ArrayList<Etudiant>): A
         rowview.findViewById<TextView>(R.id.textViewPrenom).text = liste[position].prenom
         rowview.findViewById<TextView>(R.id.textViewnNom).text = liste[position].nom
         rowview.findViewById<TextView>(R.id.textViewAge).text = liste[position].age.toString()
+        rowview.findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.bob)
 
         return rowview
     }
